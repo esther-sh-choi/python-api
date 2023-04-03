c9a178c52e1a9fe01a966ecdb5ef76382ab58b84
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-03 14:52:39 -0400

    Add gitignore

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import flask
+from flask import Flask
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_ghw():
-    return "<p>Hello, API Week Hackers!</p>"
+    return "<p>Hello, My name is Esther!</p>"
+
+if __name__=="__main__":
+    app.run(debug.True)
+
+    
