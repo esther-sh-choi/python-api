0a27b60753f86c766ccddec5a5be213987cfb186
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-04-03 15:15:58 -0400

    Add requirement file

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_ghw():
     return "<p>Hello, My name is Esther!</p>"
 
+@app.route('/getHackathons', methods=['GET'])
+def getHackathons():
+  return hackathons_list
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug = True)
 
